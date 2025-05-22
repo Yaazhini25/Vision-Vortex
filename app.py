aa7cebd87c0c32c308bfd748883a8da5986269c2
YAAZHINI S <yaazhssk7@gmail.com>
2025-05-22 16:31:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,8 @@ def predict():
         "label": "Fake" if prediction else "Real",
         "confidence": round(prob, 2)
     })
-
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
